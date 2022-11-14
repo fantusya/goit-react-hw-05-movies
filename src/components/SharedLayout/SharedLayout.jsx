0bb8508ceb275f9f0eab0f95e4892182ed51d954
Anastasiia <guba.anastaysha@gmail.com>
2022-11-14 21:10:11 +0200

    updated

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/movies">Movies</NavLink>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
+  );
+};
+
+export default SharedLayout;
