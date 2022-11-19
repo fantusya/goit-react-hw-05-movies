06fd2f7ae888b87858bd6800ed9ae1b0795f2957
Anastasiia <guba.anastaysha@gmail.com>
2022-11-19 20:39:32 +0200

    added new constants

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,10 @@
 import errorCast from 'images/fallback.jpg';
 import errorPoster from 'images/posterFallback.jpg';
 
+export const api_key = `e09f06c48afcb3ebfd8a25b0b6965d1e`;
+
+export const BASE_GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list';
+
 export const BASE_POSTER_URL = `https://image.tmdb.org/t/p/w500/`;
 
 export const FALLBACK_IMAGE_PATH = errorPoster;
