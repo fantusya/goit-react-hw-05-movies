527f6835e1c751e9dda803faac5ffca2374ee414
Anastasiia <guba.anastaysha@gmail.com>
2022-11-19 20:35:02 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -6,17 +6,9 @@ import { theme } from 'constants/theme';
 import { App } from 'components/App';
 import './index.css';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter basename="/goit-react-hw-05-movies">
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
