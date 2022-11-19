6da6b63072b9ef884aac4d3308da093134ec41f1
Anastasiia <guba.anastaysha@gmail.com>
2022-11-19 12:18:57 +0200

    common urls

---
+++ src/constants/urls.js
@@ -0,0 +1,8 @@
+import errorCast from 'images/fallback.jpg';
+import errorPoster from 'images/posterFallback.jpg';
+
+export const BASE_POSTER_URL = `https://image.tmdb.org/t/p/w500/`;
+
+export const FALLBACK_IMAGE_PATH = errorPoster;
+
+export const FALLBACK_CAST_IMAGE_PATH = errorCast;
